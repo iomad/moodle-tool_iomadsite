dabab48a276db9194a610540b66384f92a95054c
Derick Turner <derick@e-learndesign.co.uk>
2025-08-26 21:20:06 +0100

    IOMAD: using point releases won't work so trying a different method to ensure different version numbers missed some

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release  = '4.5.6 (Build: 20250811)'; // Human-friendly version name
-$plugin->version  = 2024100701;   // The (date) version of this plugin.
+$plugin->version  = 2024100745;   // The (date) version of this plugin.
 $plugin->requires = 2024100700;   // Requires this Moodle version.
 $plugin->component = 'tool_iomadsite';
 $plugin->dependencies = ['local_iomad' => 2024090401];
